d082f8644ea93364d8ff7210afd703c8652d04c0
Tim <hoilonuzero@gmail.com>
2022-06-28 02:10:15 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function reverse (n) {
-    let b=String(n);
+    let b=String(Math.abs(n));
     let a='';
     for (var i=0; i<b.length; i++) {
         a += b[b.length-i-1];
     }
-    return Math.abs(a/1);
+    return a/1;
 }
